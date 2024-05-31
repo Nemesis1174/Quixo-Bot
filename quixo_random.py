1b95a3b64c8ac75412a68084da3890c3a3fa7aa5
Christopher Oswaldo Márquez Reyes <145897791+Nemesis1174@users.noreply.github.com>
2024-05-31 14:13:27 -0600

    Update quixo_random.py

--- quixo_random.py
+++ quixo_random.py
@@ -1,3 +1,4 @@
+from tabulate import tabulate
 import random
 import copy
 
@@ -6,40 +7,28 @@ class QuixoRandomBot:
     ALLOWED_PIECES_LEFT = [(0, 1), (0, 2), (0, 3), (1, 4), (2, 4), (3, 4), (4, 4), (4, 1), (4, 2), (4, 3), (4, 4)]
     ALLOWED_PIECES_UP = [(1, 0), (1, 4), (2, 0), (2, 4), (3, 0), (3, 4), (4, 0), (4, 1), (4, 2), (4, 3), (4, 4)]
     ALLOWED_PIECES_DOWN = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (1, 4), (2, 0), (2, 4), (3, 0), (3, 4)]
+    ALLOWED_PIECES_GENERAL = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (1, 4), (2, 0), (2, 4), (3, 0), (3, 4), (4, 0), (4, 1), (4, 2), (4, 3), (4, 4)]
 
     def __init__(self, symbol):
-        self.name = "QuixoRandomBot"
         self.symbol = symbol
-        self.opponent_symbol = -symbol
+        self.opponent_symbol = 1 if symbol == -1 else -1
+        self.board = [[0] * 5 for _ in range(5)]
+        self.name = "Kuri Bot Random"
 
     def play_turn(self, board):
-        valid_moves = []
-
-        for i in range(5):
-            for j in range(5):
-                if self.is_legal_move(i, j, board):
-                    possible_moves = self.get_possible_moves(board, i, j, self.symbol)
-                    valid_moves.extend(possible_moves)
-
+        valid_moves = self.generate_moves(board, self.symbol)
         if valid_moves:
-            return random.choice(valid_moves)
+            move = random.choice(valid_moves)
+            direction, (row, col) = move
+            if direction == 'right':
+                self.move_right(board, row, col)
+            elif direction == 'left':
+                self.move_left(board, row, col)
+            elif direction == 'up':
+                self.move_up(board, row, col)
+            elif direction == 'down':
+                self.move_down(board, row, col)
         return board
-
-    def is_legal_move(self, row, col, board):
-        allowed_positions = self.ALLOWED_PIECES_GENERAL
-        return (row, col) in allowed_positions and (board[row][col] in [0, self.symbol])
-
-    def get_possible_moves(self, board, row, col, symbol):
-        new_boards = []
-        if row == 0:
-            new_boards.append(self.move_down(copy.deepcopy(board), row, col))
-        elif row == 4:
-            new_boards.append(self.move_up(copy.deepcopy(board), row, col))
-        if col == 0:
-            new_boards.append(self.move_right(copy.deepcopy(board), row, col))
-        elif col == 4:
-            new_boards.append(self.move_left(copy.deepcopy(board), row, col))
-        return new_boards
 
     def move_right(self, board, row, col, end_col=4):
         piece = board[row][col]
@@ -48,7 +37,6 @@ class QuixoRandomBot:
                 for i in range(col, end_col):
                     board[row][i] = board[row][i + 1]
                 board[row][end_col] = self.symbol
-        return board
 
     def move_left(self, board, row, col, end_col=0):
         piece = board[row][col]
@@ -57,7 +45,6 @@ class QuixoRandomBot:
                 for i in range(col, end_col, -1):
                     board[row][i] = board[row][i - 1]
                 board[row][end_col] = self.symbol
-        return board
 
     def move_up(self, board, row, col, end_row=0):
         piece = board[row][col]
@@ -66,7 +53,6 @@ class QuixoRandomBot:
                 for i in range(row, end_row, -1):
                     board[i][col] = board[i - 1][col]
                 board[end_row][col] = self.symbol
-        return board
 
     def move_down(self, board, row, col, end_row=4):
         piece = board[row][col]
@@ -75,15 +61,61 @@ class QuixoRandomBot:
                 for i in range(row, end_row):
                     board[i][col] = board[i + 1][col]
                 board[end_row][col] = self.symbol
-        return board
 
-    def print_board(self, board):
-        for row in board:
-            print(" ".join(str(cell) for cell in row))
-        print()
+    def print_board(self, board=None):
+        if board is None:
+            board = self.board
+        headers = [""] + [str(i) for i in range(1, 6)]
+        rows = [[str(i + 1)] + ['O' if cell == -1 else 'X' if cell == 1 else ' ' for cell in row] for i, row in enumerate(board)]
+        print(tabulate(rows, headers=headers, tablefmt="grid"))
 
     def reset(self, symbol):
         self.symbol = symbol
-        self.opponent_symbol = -symbol
+        self.opponent_symbol = 1 if symbol == -1 else -1
+        self.board = [[0] * 5 for _ in range(5)]
 
+    def is_winner(self, board, symbol):
+        lines = [board[i] for i in range(5)] + [[board[j][i] for j in range(5)] for i in range(5)]
+        lines.append([board[i][i] for i in range(5)])
+        lines.append([board[i][4 - i] for i in range(5)])
+        for line in lines:
+            if all(cell == symbol for cell in line):
+                return True
+        return False
 
+    def is_full(self, board):
+        return all(cell != 0 for row in board for cell in row)
+
+    def generate_moves(self, board, symbol):
+        moves = []
+        allowed_positions = self.ALLOWED_PIECES_GENERAL
+        for direction in ['right', 'left', 'up', 'down']:
+            for row, col in allowed_positions:
+                if board[row][col] == 0 or board[row][col] == symbol:
+                    if self.is_valid_move(board, direction, row, col, symbol):
+                        moves.append((direction, (row, col)))
+        return moves
+
+    def is_valid_move(self, board, direction, row, col, symbol):
+        if direction == 'right' and col < 4 and (board[row][col + 1] == 0 or board[row][col + 1] == symbol):
+            return True
+        if direction == 'left' and col > 0 and (board[row][col - 1] == 0 or board[row][col - 1] == symbol):
+            return True
+        if direction == 'up' and row > 0 and (board[row - 1][col] == 0 or board[row - 1][col] == symbol):
+            return True
+        if direction == 'down' and row < 4 and (board[row + 1][col] == 0 or board[row + 1][col] == symbol):
+            return True
+        return False
+
+    def apply_move(self, board, move, symbol):
+        new_board = copy.deepcopy(board)
+        direction, (row, col) = move
+        if direction == 'right':
+            self.move_right(new_board, row, col)
+        elif direction == 'left':
+            self.move_left(new_board, row, col)
+        elif direction == 'up':
+            self.move_up(new_board, row, col)
+        elif direction == 'down':
+            self.move_down(new_board, row, col)
+        return new_board
